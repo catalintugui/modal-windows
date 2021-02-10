e742d45930431c2cc026ddb14f0371935e4a5c64
Catalin Tugui <katamaxx23@gmail.com>
2021-02-10 16:45:21 +0200

    dry js

--- script.js
+++ script.js
@@ -5,23 +5,22 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 
-const addClasses = () => {
+const closeModal = () => {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
-const removeClasses = () => {
+const openModal = () => {
     modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
 }
 
 for(let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', removeClasses)
+    btnsOpenModal[i].addEventListener('click', openModal)
 }
 
-btnCloseModal.addEventListener('click', addClasses);
+btnCloseModal.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', addClasses)
-
+overlay.addEventListener('click', closeModal);
 
 
