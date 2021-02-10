0645e9df9c750122b577c3ff7aad751a9b0e2e1f
Catalin Tugui <katamaxx23@gmail.com>
2021-02-10 17:20:46 +0200

    escape added

--- script.js
+++ script.js
@@ -23,4 +23,9 @@ btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function(event){
+    if(event.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+})
 
